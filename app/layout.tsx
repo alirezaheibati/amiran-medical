9797f18541b23122d16022a86180e1f81946f27b
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-06-06 20:10:43 +0330

    chore (app/layout.tsx): set favicon property path in layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { El_Messiri, Vazirmatn } from "next/font/google";
 import "./globals.css";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 const elMessiri = El_Messiri({
   subsets: ["latin"],
   weight: ["400", "700"],
@@ -13,6 +17,9 @@ const vazirmatn = Vazirmatn({
 export const metadata: Metadata = {
   title: "Amirean medical",
   description: "Amiran Medical pharmaceutical shop active in Zanjan province",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
