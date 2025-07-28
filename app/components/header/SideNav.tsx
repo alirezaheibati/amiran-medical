6da472c5cb0356c7cba40b2b5a11915d37ab2024
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-07-28 20:24:15 +0330

    feat (app/components/header): Implement SideNav component as side navigation drawer

---
+++ app/components/header/SideNav.tsx
@@ -0,0 +1,46 @@
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NavItem from "./NavItem";
+
+interface SideNavProps {
+  showNav: boolean;
+  onCloseNav: () => void;
+}
+/**
+ * Mobile-friendly side navigation drawer.
+ * Includes a semi-transparent overlay and a list of localized navigation links.
+ * Visibility toggled with smooth translation animations.
+ *
+ * @component
+ * @param {SideNavProps} props - Props to control navigation visibility and closing
+ * @returns {JSX.Element} A responsive side navigation component
+ */
+export default function SideNav({ showNav, onCloseNav }: SideNavProps) {
+  return (
+    <div
+      className={`fixed h-screen w-screen top-0 bottom-0 right-0 z-20 ${
+        showNav ? "translate-x-0" : "translate-x-full"
+      }`}
+    >
+      <div className="w-full h-full absolute top-0 right-0 bg-black/50"></div>
+      <ul
+        className={`flex flex-col h-full justify-start items-start gap-4 bg-white w-full max-w-sm px-8 py-16 relative shadow-md transition-transform duration-300 ${
+          showNav ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <button
+          className="absolute left-8 top-8 text-[#003d5d] cursor-pointer"
+          onClick={onCloseNav}
+        >
+          <FontAwesomeIcon icon={faXmark} className="text-3xl" />
+        </button>
+        <NavItem href="/" title="خانه" isActive />
+        <NavItem href="#" title="تخفیف دارها" isActive={false} />
+        <NavItem href="#" title="نصب اپلیکیشن" isActive={false} />
+        <NavItem href="#" title="راهنمای خرید" isActive={false} />
+        <NavItem href="#" title="قوانین" isActive={false} />
+        <NavItem href="#" title="تماس با ما" isActive={false} />
+      </ul>
+    </div>
+  );
+}
