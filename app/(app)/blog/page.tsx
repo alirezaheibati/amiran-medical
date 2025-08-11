fb1e50cffb776280260beb88a7ff0c8c94e48d45
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-08-11 17:35:42 +0330

    feat (app/(app)/blog/page.tsx): Implement BlogPage component that renders the main blog pag

---
+++ app/(app)/blog/page.tsx
@@ -0,0 +1,30 @@
+import { getBlogPostsCount, getPostsFirstPage } from "@/app/_lib/posts";
+import { convertToPersianDigits } from "@/app/_utils/convertToPersianDigits";
+import BlogPostsGrid from "@/app/components/blog/BlogPostsGrid";
+/**
+ * Server-side component that renders the main blog page.
+ *
+ * Fetches the total number of blog posts and the first page of posts from the database.
+ * Displays a header with the blog title and post count (converted to Persian digits),
+ * followed by a grid of blog post cards with pagination.
+ *
+ * @component
+ * @returns {Promise<JSX.Element>} The rendered blog page section.
+ */
+export default async function BlogPage() {
+  const allPostsCount = await getBlogPostsCount();
+  const blogPosts = await getPostsFirstPage();
+  return (
+    <section className="max-w-7xl mx-auto px-4 py-8">
+      <div className="flex justify-between items-center py-4 text-[#003d5d]">
+        <h1 className="font-semibold text-2xl font-messiri">پست های وبلاگ</h1>
+        <p className="flex justify-start items-center gap-0.5">
+          <span className="font-semibold">{convertToPersianDigits(12)}</span>
+          <span>مطلب موجود است.</span>
+        </p>
+      </div>
+      <hr className="border-2 border-[#003d5d]/20 mb-8" />
+      <BlogPostsGrid blogPosts={blogPosts} allPostsCount={allPostsCount} />
+    </section>
+  );
+}
