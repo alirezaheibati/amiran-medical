4b345a7608863fbf7c04cafd68ef29c0e8f24b9c
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-08-17 21:21:37 +0330

    refactor (app/components/blog/BlogPostsGrid): Refactor grid layout. drop xl breakpoint

--- app/components/blog/BlogPostsGrid.tsx
+++ app/components/blog/BlogPostsGrid.tsx
@@ -47,7 +47,7 @@ export default function BlogPostsGrid({
 
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {posts.map((post) => (
           <BlogPostCard key={post.id} post={post} />
         ))}
