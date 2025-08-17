f797224faeb7f12f661d976d869cb64f5220b292
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-08-17 21:19:29 +0330

    refactor (app/components/blog/BlogPostCard): refactor href attribute of post card link

--- app/components/blog/BlogPostCard.tsx
+++ app/components/blog/BlogPostCard.tsx
@@ -1,4 +1,5 @@
 import { BlogPostType } from "@/app/_interface/BlogPostType";
+import { mapPostCategoryToPersian } from "@/app/_utils/mapPostCatName";
 import Image from "next/image";
 import Link from "next/link";
 interface BlogPostCardProps {
@@ -28,12 +29,14 @@ export default function BlogPostCard({ post }: BlogPostCardProps) {
         <h2 className="line-clamp-1 font-semibold">{post.title}</h2>
         <div className="flex justify-between items-center pt-4">
           <Link
-            href={`/blog/${post.slug}`}
+            href={`/blog/${post.category}/${post.slug}`}
             className=" bg-[#003d5d] text-white/90 text-sm border-2 rounded-md hover:scale-105 px-4 py-2 transition-transform text-center"
           >
             مشاهده
           </Link>
-          <p className="text-[#003d5d]/50 text-xs">{post.category}</p>
+          <p className="text-[#003d5d]/50 text-xs">
+            {mapPostCategoryToPersian(post.category)}
+          </p>
         </div>
       </div>
     </div>
