a1cbcf03d56b475faa0809381fb1fc1911cd9ace
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-08-20 16:52:34 +0330

    feat (app/components/blog): Implement PostCategories component to asynchronously fetches and displays a list of post categories

---
+++ app/components/blog/PostCategories.tsx
@@ -0,0 +1,27 @@
+import { getPostCategories } from "@/app/_lib/posts";
+import PostCategoryItem from "./PostCategoryItem";
+/**
+ * Asynchronously fetches and displays a list of post categories.
+ *
+ * This component retrieves post categories from the backend using `getPostCategories`,
+ * and renders them as a list of `PostCategoryItem` components.
+ *
+ * @component
+ * @async
+ * @returns {JSX.Element} A styled container with a heading and a list of post categories.
+ */
+export default async function PostCategories() {
+  const categories = await getPostCategories();
+
+  return (
+    <div className="rounded-lg border border-gray-200 p-4">
+      <h1 className="font-semibold font-messiri">دسته بندی مطالب</h1>
+      <hr className="mb-4 mt-3" />
+      <ul className="flex flex-col justify-start items-start gap-2">
+        {categories.map((category) => (
+          <PostCategoryItem category={category} key={category.category} />
+        ))}
+      </ul>
+    </div>
+  );
+}
