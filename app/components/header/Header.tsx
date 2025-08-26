094d20474fa76cc84e41f3c92f88a0d418eef112
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-08-26 18:57:57 +0330

    refactor (app/components/header/Header.tsx): convert Header to server component and integrate LogoutBtn

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,13 +1,12 @@
-"use client";
 import Link from "next/link";
 import Branding from "./Branding";
 import CallSupport from "./CallSupport";
 import HeaderCartLink from "./HeaderCartLink";
 import SearchBar from "./SearchBar";
-import { useState } from "react";
-import ShowNavBtn from "./ShowNavBtn";
-import SideNav from "./SideNav";
 import TopNav from "./TopNav";
+import { User } from "lucia";
+import NavContainer from "./NavContainer";
+import LogoutBtn from "./LogoutBtn";
 /**
  * Header component for the application layout.
  * Combines brand elements, navigation (responsive and localized), search, support, and authentication entry.
@@ -16,16 +15,10 @@ import TopNav from "./TopNav";
  * @component
  * @returns {JSX.Element} A complete header layout with navigation and hero
  */
-export default function Header() {
-  const [showNav, setShowNav] = useState(false);
-  // Show mobile side navigation
-  function showNavHandler() {
-    setShowNav(true);
-  }
-  // Hide mobile side navigation
-  function hideNavHandler() {
-    setShowNav(false);
-  }
+interface HeaderProps {
+  loggedInUser: User | null;
+}
+export default function Header({ loggedInUser }: HeaderProps) {
   return (
     <header>
       <div className="max-w-7xl mx-auto p-4">
@@ -41,13 +34,14 @@ export default function Header() {
       {/* top navigation */}
       <nav className="text-[#003d5d] py-4 bg-gray-50">
         <div className="flex justify-between items-center max-w-7xl mx-auto px-4">
-          {/* menu bars btn */}
-          <ShowNavBtn OnPressBtn={showNavHandler} />
-          {/* side nav for small screens */}
-          <SideNav onCloseNav={hideNavHandler} showNav={showNav} />
+          <NavContainer />
           {/* nav for +768 screens */}
           <TopNav />
-          <Link href={"/auth"}>ورود / ثبت نام</Link>
+          {loggedInUser ? (
+            <LogoutBtn />
+          ) : (
+            <Link href={"/auth"}>ورود / ثبت نام</Link>
+          )}
         </div>
       </nav>
     </header>
