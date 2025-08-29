9858850c21dcb4baae30206128a36ee4d9448f4b
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-08-29 16:51:13 +0330

    refactor (app/components/header/SideNav.tsx): add blog navigation item to SideNav component

--- app/components/header/SideNav.tsx
+++ app/components/header/SideNav.tsx
@@ -36,6 +36,7 @@ export default function SideNav({ showNav, onCloseNav }: SideNavProps) {
         </button>
         <NavItem href="/" title="خانه" />
         <NavItem href="/shop" title="فروشگاه" />
+        <NavItem href="/blog" title="وبلاگ" />
         <NavItem href="/cooperate" title="همکاری در فروش" />
         <NavItem href="/faq" title="پرسش‌های متداول" />
         <NavItem href="/rules" title="قوانین" />
